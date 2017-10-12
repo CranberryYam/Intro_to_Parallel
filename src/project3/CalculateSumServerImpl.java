cebffc67d0f1de3fd924d9d878cbc57ef9a6d503
Henry Yi <1113145615yihl@gmail.com>
2017-10-12 18:44:05 -0400

    finish project3

---
+++ src/project3/CalculateSumServerImpl.java
@@ -0,0 +1,26 @@
+package project3;
+
+import java.rmi.*;
+import java.rmi.server.*;
+
+public class CalculateSumServerImpl extends UnicastRemoteObject implements Project4ServerInterface {
+
+	private static final long serialVersionUID = 1L;
+
+	public CalculateSumServerImpl() throws RemoteException {
+		super();
+		System.out.println("new Impl() object"); 
+	}
+
+	@Override
+	public int calculateSum(int n) throws RemoteException {
+		int product = 1;
+		if(n>0) {
+			for(int i=1;i<=n;i++) {
+				product = product*i;
+			}
+		}
+		return product;
+	}
+}
+
